e51d44a9a604bb8b668a8d8574e661ea17ec9539
aplikadote <aplikadito@gmail.com>
2024-09-17 23:36:19 -0300

    dia 1 aruspice

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
 call python .\pyscripts\encode_keys.py -i Strings/main.dat.xml -o Strings/main.dat
+call python .\pyscripts\encode_keys.py -i Strings_dev/main.dat.xml -o Strings_dev/main.dat
 
 exit
---
+++ deploy_dev.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+call robocopy .\Strings_dev "D:\juegos\Steam\steamapps\common\Pathologic Classic HD\data\Strings"
+
+exit
