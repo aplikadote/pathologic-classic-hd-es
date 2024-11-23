65d75149508dd19a873455e099b6886d15eb5e16
rgonzalez <aplikadito@gmail.com>
2024-11-23 12:19:28 -0300

    intento 01 de correccion bug ???

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 rem Codificando 'Strings\main.dat'
-call python .\pyscripts\encode_keys.py -i Strings/main.dat.xml -o Strings/main.dat
+call py .\pyscripts\encode_keys.py -i Strings/main.dat.xml -o Strings/main.dat
 
 rem Instalando directorio 'Strings'
 call robocopy .\Strings "%PathologicGameDir%\data\Strings"
